a20d326c2fe725158d2d7a0742ab77fba6db6e63
Vikas Bhandari <vikas@triamis.com>
2018-02-08 18:37:34 +0800

    map for itinerary created

---
+++ gitTest/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  gitTest
+//
+//  Created by Vikas Bhandari on 8/2/2018.
+//  Copyright © 2018 Vikas Bhandari. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
